710c1c4777ed52bf9528f512f6643ce34ea9ce19
john so <31013498+johnrso@users.noreply.github.com>
2020-07-03 13:10:01 -0700

    added generator control

--- arduino/control/control.ino
+++ arduino/control/control.ino
@@ -6,14 +6,21 @@ int NUM_DISP; // number of dispensers
 int SOLENOIDS[NUM_SOLENOIDS]; // array of solenoid pins
 int solenoidStates[NUM_SOLENOIDS]; // array of solenoid states, 1 = closed, 0 = open
 
+int PUMP; // pump motor
 int DISPS[NUM_DISP]; // array of water level sensors
-float solenoidStatus[NUM_SOLENOIDS]; // array of sensor readings
+float levelReadings[NUM_DISP]; // array of sensor readings
 
-unsigned long int GENERATION_TIME; // 
+unsigned long GENERATION_TIME; // time for HClO generation to run 
+unsigned long GENERATION_DOWN_TIME; // time in between HClO generation
+
+float ACC_LEVEL_THRESHOLD; // acceptable level of water to fill 
+float UNACC_LEVEL_THRESHOLD; //level of water to signal a refill
 
 int GENERATOR; // pin for controlling the generation of HCLO
 
+int genStatus; // boolean state of the generator
 unsigned long lastGeneration; // variable for tracking the last time when the generator was powered.
+unsigned long genOn; // variable for the time at which the generator was powered on.
 
 void setup() {
   Serial.begin(57600); // communication through serial port
@@ -27,12 +34,35 @@ void setup() {
   }
 
   pinMode(GENERATOR, OUTPUT);
-  lastGeneration = 0;
+  lastGeneration = 0; // look into making last generation 
+  genStatus = 0;
+  genOn = 0;
 }
 
 void loop() {
-  while(1) {
-    unsigned long currentTime = millis();
+  
+  unsigned long currentTime;
+  delay(50); // ensure that current time is not 0
+  
+  while(currentTime = millis()) {
+  
+    if (genStatus) { // if generator on
+      if ((currentTime - genOn) >= GENERATION_TIME) { // if generator has been on for long enough
+        lastGeneration = curentTime;
+        digitalWrite(GENERATOR, LOW);
+        genStatus = 1 - genStatus;
+      }
+    } else {
+      if ((currentTime - lastGeneration) >= GENERATION_DOWN_TIME) { // if generator has been off long enough
+        genOn = curentTime;
+        digitalWrite(GENERATOR, HIGH);
+        genStatus = 1 - genStatus;
+      }
+    }
+
+    for (int i = 0; i < NUM_DISP; i++) {
+      DISPS[i] = analogRead(
+    }
     
   }
 }
